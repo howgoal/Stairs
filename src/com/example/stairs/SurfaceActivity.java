b86872ce533d28712559c5ee587175fee35b2099
outxian <sakaur00@gmail.com>
2015-05-03 15:29:17 +0800

    with sensor

--- src/com/example/stairs/SurfaceActivity.java
+++ src/com/example/stairs/SurfaceActivity.java
@@ -1,6 +1,7 @@
 package com.example.stairs;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -11,14 +12,22 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.ImageView;
 
 public class SurfaceActivity extends SurfaceView implements
-		SurfaceHolder.Callback {
+		SurfaceHolder.Callback, SensorEventListener {
 
 	private SurfaceHolder surfHold;
+	private SensorManager sensorManager;
+	private boolean accelerometerPresent;
+	private Sensor accelerometerSensor;
 	private Timer timer;
 	private TimerTask task;
 	private int xPosition, yPosition;
@@ -28,11 +37,10 @@ public class SurfaceActivity extends SurfaceView implements
 	private int speedUp = 1, speedDown = 1;
 	private int gap = 250;
 	private int tmp_hit = 0, hit = 0;
-	private boolean change = false;
-	
-	ArrayList <Integer> xList;
-	ArrayList <Integer> yList;
+	private boolean direct = true;
 
+	ArrayList<Integer> xList;
+	ArrayList<Integer> yList;
 
 	public SurfaceActivity(Context context) {
 		super(context);
@@ -50,35 +58,35 @@ public class SurfaceActivity extends SurfaceView implements
 
 	private void countX() {
 		xList.add(130);
-		for(int i=1; i<50; i++) {
+		for (int i = 1; i < 50; i++) {
 			xPosition = (int) (Math.random() * 300 + 10);
 			xList.add(xPosition);
 		}
 	}
-	
+
 	private void countY() {
 		yList.add(gap);
-		for(int i=1; i<50; i++) {
+		for (int i = 1; i < 50; i++) {
 			gap = gap + 100;
-			yPosition = (int) (Math.random() * 30 )+ gap;
+			yPosition = (int) (Math.random() * 30) + gap;
 			yList.add(yPosition);
 		}
 	}
-	
+
 	public void checkOn() {
-		for(int i=0; i<10; i++) {
-			if(Math.abs(ySheep+hSheep - yList.get(i)) < 2) {
-				if(xSheep-xList.get(i) <= wSteps-20 && -hSheep+30 <= xSheep-xList.get(i)) {
-					Log.i("up", "up");
+		for (int i = 0; i < 10; i++) {
+			if (Math.abs(ySheep + hSheep - yList.get(i)) < 2) {
+				if (xSheep - xList.get(i) <= wSteps - 20
+						&& -hSheep + 30 <= xSheep - xList.get(i)) {
+					//Log.i("up", "up");
 					ySheep -= speedUp;
 					hit++;
 				}
 			}
 		}
-		if(hit == tmp_hit) {
+		if (hit == tmp_hit) {
 			ySheep += speedDown;
-		}
-		else{
+		} else {
 			tmp_hit = hit;
 		}
 	}
@@ -87,12 +95,20 @@ public class SurfaceActivity extends SurfaceView implements
 		Canvas canvas = getHolder().lockCanvas();
 		Resources res = getResources();
 		Bitmap steps = BitmapFactory.decodeResource(res, R.drawable.img);
-		Bitmap sheep = BitmapFactory.decodeResource(res, R.drawable.sheep);
+		Bitmap left_sheep = BitmapFactory.decodeResource(res, R.drawable.left_sheep);
+		Bitmap right_sheep = BitmapFactory.decodeResource(res, R.drawable.right_sheep);
 		Paint paint = new Paint();
 		paint.setAntiAlias(true); // remove edge effect
 		canvas.drawColor(Color.WHITE);
-		canvas.drawBitmap(sheep, xSheep, ySheep, paint);
-		for(int i=0; i<10; i++) {
+		
+		if(direct == true) {
+			canvas.drawBitmap(right_sheep, xSheep, ySheep, paint);
+		}
+		else {
+			canvas.drawBitmap(left_sheep, xSheep, ySheep, paint);
+		}
+		
+		for (int i = 0; i < 10; i++) {
 			canvas.drawBitmap(steps, xList.get(i), yList.get(i), paint);
 		}
 
@@ -105,12 +121,12 @@ public class SurfaceActivity extends SurfaceView implements
 			@Override
 			public void run() {
 				draw();
-				for(int i=0; i<yList.size(); i++) {
+				for (int i = 0; i < yList.size(); i++) {
 					yList.set(i, yList.get(i) - speedUp);
-					if(yList.get(i) < 0) {
+					if (yList.get(i) < 0) {
 						yList.remove(i);
 						xList.remove(i);
-						//Log.i("remove", "ok");
+						// Log.i("remove", "ok");
 					}
 				}
 				checkOn();
@@ -123,6 +139,7 @@ public class SurfaceActivity extends SurfaceView implements
 		timer.cancel();
 	}
 
+
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width,
 			int height) {
@@ -133,6 +150,12 @@ public class SurfaceActivity extends SurfaceView implements
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
 		// TODO Auto-generated method stub
+		sensorManager = (SensorManager) getContext().getSystemService(
+				Context.SENSOR_SERVICE);
+		 sensorManager.registerListener(this,
+			        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+			        SensorManager.SENSOR_DELAY_NORMAL);
+
 		startTimer();
 	}
 
@@ -140,6 +163,43 @@ public class SurfaceActivity extends SurfaceView implements
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		// TODO Auto-generated method stub
 		stopTimer();
+		sensorManager.unregisterListener(this);
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		// TODO Auto-generated method stub
+
+		if (event.values[0] > 1) {
+			// x>0 left
+			direct = false;
+			if(xSheep > 15) {
+				xSheep -= 15;
+			}
+		}
+		if (event.values[0] < -1) {
+			// x<0 right
+			direct = true;
+			if(xSheep < 385) {
+				xSheep += 15;
+			}
+		}
+		if (event.values[1] > 1) {
+			// y>0 down
+//			ySheep += 5;
+//			Log.i("123", "DOWN");
+		}
+		if (event.values[1] < -1) {
+			// y<0 up
+//			ySheep -= 5;
+//			Log.i("123", "UP");
+		}
 	}
 
 }
